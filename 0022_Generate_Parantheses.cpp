6ade13810c8f5d4a89e7fd0d22ba97a92d36de7e
tatusamel <55717583+tatusamel@users.noreply.github.com>
2022-10-19 08:24:48 +0300

    Update 0022_Generate_Parantheses.cpp

--- 0022_Generate_Parantheses.cpp
+++ 0022_Generate_Parantheses.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     
-    void helper(int open, int closed, int n, const string& str, vector<string>& res) {
+    // we need to do backtracking so we will not return at the end
+    // we'll just modify the result array
+    void helper(int open, int closed, int n, const string& str, vector<string>& res){
         
-        if ( str.size() == 2*n ) {
+        // base case
+        if ( str.size() == 2 * n ){
             res.push_back(str);
             return;
         }
-        if ( open < n ) helper(open+1, closed, n, str+"(", res);
-        if ( closed < open )    helper(open, closed+1, n , str+')', res);
+        // if number of open bracket is less than n, we can add more open
+        if ( open < n ) helper(open+1, closed, n, str+'(' , res);
         
+        // if number of closed bracket is less than open bracket, we can add more closed
+        if ( closed < open ) helper(open, closed+1, n, str+')', res);
+        
+        // make sure to not return inside those if statements because we need to do backtracking
     }
     
     vector<string> generateParenthesis(int n) {
